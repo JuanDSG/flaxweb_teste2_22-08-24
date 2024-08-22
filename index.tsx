02a9376a5943be4f2a426d04e18c3c8dcbcc33fa
JuanDSG <juandiegosg@gmail.com>
2024-08-22 20:24:15 -0300

    Commit generated by Flaxboll.

--- index.tsx
+++ index.tsx
@@ -1,6 +1,5 @@
 
-
-        // ---------- import React Packs
+      // ---------- import React Packs
         import React from 'react';
         import * as RN from 'react-native';
 
@@ -8,47 +7,23 @@
         import { create } from 'zustand';
 
         // ---------- import Local Tools
-        import {mapElements} from './tools/base/project/mapElements';
-        import * as functions from './tools/base/functions';
-        import * as Elements from './tools/base/Elements';
-        import { Project } from './tools/base/project/';
-        import * as jsvals from './tools/base/jsvals';
-        import { props } from './tools/base/props';
-        import * as customs from './tools/customs';
-        import * as stls from './tools/base/stls';
-        import { tools } from "./tools";
 
         // ---------- set Caps Inputs
-        const currRoute = 'home'
+        const currRoute = ''
 
         let args:any = []
 
-        const screens = [
-          []
-        ]
+        const screens = []
 
-        const initCt = () => ({
-          true: "true"
-        });
+        const initCt = () => ({});
         const initObj = initCt();
-        // console.log(initObj);
 
-        const arrInitFuncs = [
-          ()=>{}
-        ];
+        const arrInitFuncs = [];
 
         export const useRoutes = create(() => ({ currRoute }));
         export const useData = create(() => initObj);
 
         // ---------- set Main Component
         export const Router = () => {
-          return (
-            <Project
-              configData={{
-                screens,
-                initCt,
-                arrInitFuncs,
-              }}
-            />
-          );
-        };
+          return (<RN.Text>Base Project...</RN.Text>);
+      };
