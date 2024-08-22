9f398627d29c94a865d074ffaac71d6f8824c050
JuanDSG <juandiegosg@gmail.com>
2024-08-22 19:33:30 -0300

    Commit generated by Flaxboll.

--- src/tools/base/project/getCtData.tsx
+++ src/tools/base/project/getCtData.tsx
@@ -1,2 +1,11 @@
 
-"getCtData.tsx"
+// ---------- import Local Tools
+import { useData } from '../../../';
+import { pathSel } from './pathSel';
+
+export const getCtData = (path: string) => {
+  const ct = useData.getState();
+  const varValue = pathSel(ct, path);
+  return varValue;
+};
+
