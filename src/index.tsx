5a16da8527572fbc61eb8d796ef5e77485ea4dcf
JuanDSG <juandiegosg@gmail.com>
2024-08-22 22:00:21 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -13,9 +13,7 @@
           import { mapElements } from './tools/base/project/mapElements';
           import * as functions from './tools/base/functions';
           import * as Elements from './tools/base/Elements';
-          // import * as Elements from './src/tools/base/Elements';
           import { Project } from './tools/base/project';
-          // import { Project } from './src/tools/base/project/index.tsx';
           import * as jsvals from './tools/base/jsvals';
           import { props } from './tools/base/props';
           import * as customs from './tools/customs';
--- src/tools/base/Elements/DynView.tsx
+++ src/tools/base/Elements/DynView.tsx
@@ -1,2 +1,28 @@
 
-"DynView.tsx"
+// ---------- import Packs
+import React from 'react';
+import { View } from 'react-native';
+
+// ---------- import Local Tools
+import { argSel, getStlValues, mapElements, pathSel } from '../project';
+import { useData } from '../../..';
+
+export const css =
+  'color: lightblue; background-color: black; font-size: 11px; padding: 2px 6px; border-radius: 3px';
+
+type Tprops = {
+  pass: {
+    elementsProperties: any;
+    styles: any;
+    variablePath: string[];
+    expectedVal: string[];
+    childrenItems: any;
+    args: any;
+  };
+};
+
+// DynView - ccc_dynView Com Esquema Join Leitura de Var. (newBase)
+export const DynView = (props: Tprops) => {
+  return <View></View>;
+};
+
