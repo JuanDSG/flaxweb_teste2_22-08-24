2f1d613cc389e43ee3705491480188a5124bd080
JuanDSG <juandiegosg@gmail.com>
2024-08-23 17:09:19 -0300

    Commit generated by Flaxboll.

--- src/tools/customs/pagarme/asyncFn/index.ts
+++ src/tools/customs/pagarme/asyncFn/index.ts
@@ -1,2 +1,10 @@
 
-"index.ts"
+export const asyncFn = (url: string, configCall: {}) => {
+  const response = fetch(url, configCall).catch(asynFnErr => {
+    console.log({ asynFnErr });
+    return asynFnErr;
+  });
+
+  return response;
+};
+
