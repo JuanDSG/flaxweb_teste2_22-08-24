a3d63fa5f7f0df0cda2c52d78096f2cae6771218
JuanDSG <juandiegosg@gmail.com>
2024-08-22 21:14:05 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -2,23 +2,33 @@
 
 //
  
-        // ---------- import React Packs
-        import React from 'react';
-        import * as RN from 'react-native';
+          // ---------- import React Packs
+          import React from 'react';
+          import * as RN from 'react-native';
 
-        // ---------- import Variables Pack
-        import { create } from 'zustand';
+          // ---------- import Variables Pack
+          import { create } from 'zustand';
 
-        // ---------- import Local Tools
-        import { Project } from './src/tools/base/project/index.tsx';
+          // ---------- import Local Tools
+          // import { mapElements } from './tools/base/project/mapElements';
+          // import * as functions from './tools/base/functions';
+          // import * as Elements from './tools/base/Elements';
+          import * as Elements from './src/tools/base/Elements';
+          // import { Project } from './tools/base/project';
+          import { Project } from './src/tools/base/project/index.tsx';
+          // import * as jsvals from './tools/base/jsvals';
+          // import { props } from './tools/base/props';
+          // import * as customs from './tools/customs';
+          // import * as stls from './tools/base/stls';
+          // import { tools } from './tools';
 
-        // ---------- set Caps Inputs
-        const currRoute = 'home';
+          // ---------- set Caps Inputs
+          const currRoute = 'home';
 
-        let args: any = [];
+          let args: any = [];
 
-        const screens = [
-          
+          const screens = [
+            // 
 
         (...args: any) => <Elements.Screen3 pass={{
           pathScreen:"home",
@@ -52,31 +62,31 @@
 
           args,
         }}/>
-        ];
+          ];
 
-        const initCt = () => ({
-          true: "true"
-        });
-        const initObj = initCt();
-        // console.log(initObj);
+          const initCt = () => ({
+            // true: "true"
+          });
+          const initObj = initCt();
+          // console.log(initObj);
 
-        const arrInitFuncs = [
-          ()=>{}
-        ];
+          const arrInitFuncs = [
+            // ()=>{}
+          ];
 
-        export const useRoutes = create(() => ({ currRoute }));
-        export const useData = create(() => initObj);
+          export const useRoutes = create(() => ({ currRoute }));
+          export const useData = create(() => initObj);
 
-        // ---------- set Main Component
-        export const Router = () => {
-          return (
-            <Project
-              configData={{
-                screens,
-                initCt,
-                arrInitFuncs,
-              }}
-            />
-          );
-        };
+          // ---------- set Main Component
+          export const Router = () => {
+            return (
+              <Project
+                configData={{
+                  screens,
+                  initCt,
+                  arrInitFuncs,
+                }}
+              />
+            );
+          };
         
